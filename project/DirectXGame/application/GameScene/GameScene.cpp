12c2249693dfcd81d115b9a361836b40c47cee8f
fukumotokiraa <122710kisose@gmail.com>
2025-05-28 10:33:44 +0900

    GameSceneクラスを作った

---
+++ project/DirectXGame/application/GameScene/GameScene.cpp
@@ -0,0 +1,25 @@
+#include "GameScene.h"
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+}
+
+void GameScene::Initialize()
+{
+}
+
+void GameScene::Finalize()
+{
+}
+
+void GameScene::Update()
+{
+}
+
+void GameScene::Draw()
+{
+}
---
+++ project/DirectXGame/application/GameScene/GameScene.h
@@ -0,0 +1,16 @@
+#pragma once
+class GameScene
+{
+public:
+	GameScene();
+	~GameScene();
+
+	void Initialize();
+
+	void Finalize();
+
+	void Update();
+
+	void Draw();
+};
+
